1be6481eb40ba6b397a6900666255ef1f2a705d6
kimgusxo <81300338+kimgusxo@users.noreply.github.com>
2024-04-29 22:09:36 +0900

    first commit

---
+++ src/components/component/Grid.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-container>
+    <v-row>
+      <!-- 15개의 열을 생성합니다 -->
+      <v-col v-for="n in 15" :key="n" class="custom-col">Col {{ n }}</v-col>
+    </v-row>
+    <v-row>
+      <v-col v-for="n in 15" :key="n" class="custom-col">Col {{ n }}</v-col>
+    </v-row>
+    <v-row>
+      <v-col v-for="n in 15" :key="n" class="custom-col">Col {{ n }}</v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/component/TopNav.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-navigation-drawer v-model="drawer" app>
+      <v-list dense>
+        <v-list-item link v-for="item in items" :key="item.title" @click="drawer = false">
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app color="primary" dark>
+      <v-app-bar-nav-icon @click="drawer = !drawer" />
+      <v-toolbar-title>Title</v-toolbar-title>
+    </v-app-bar>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
